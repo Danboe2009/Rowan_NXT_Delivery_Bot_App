0ccf97e11a33745188e6c47a0f06f481a3d16244
Dan Boehmke <Danboe2009@gmail.com>
2016-04-11 13:25:12 -0400

    Added in Kevin's Changes

---
+++ app/src/main/java/com/boehmke/robotprototype/WaypointHistoryObserver.java
@@ -0,0 +1,23 @@
+package com.boehmke.robotprototype;
+
+import android.app.Activity;
+import android.database.DataSetObserver;
+
+/**
+ * Created by Kevin on 4/6/2016.
+ * <p/>
+ * Observer for list of waypoints.
+ */
+public class WaypointHistoryObserver extends DataSetObserver {
+
+    private WaypointHistoryActivity activity;
+
+    WaypointHistoryObserver(Activity activity) {
+        this.activity = (WaypointHistoryActivity) activity;
+    }
+
+    @Override
+    public void onChanged() {
+        activity.refreshAdapter();
+    }
+}
